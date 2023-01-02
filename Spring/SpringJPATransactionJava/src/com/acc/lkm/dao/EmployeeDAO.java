ca1eed1dfb127fc68d2782f87b965bc4bb64a283
Ishita-mukherjee <ishitamukherjee99@gmail.com>
2023-01-02 04:39:03 +0000

    Delete Spring directory

--- Spring/SpringJPATransactionJava/src/com/acc/lkm/dao/EmployeeDAO.java
+++
@@ -1,8 +0,0 @@
-package com.acc.lkm.dao;
-
-import com.acc.lkm.businessbean.EmployeeBean;
-
-public interface EmployeeDAO {
-Integer addEmployee(EmployeeBean employeeBean) throws Exception;
-EmployeeBean getEmployeeDetails(Integer id)throws Exception;
-}
--- Spring/SpringJPATransactionJava/src/com/acc/lkm/service/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.acc.lkm.service;
-
-import com.acc.lkm.businessbean.EmployeeBean;
-
-public interface EmployeeService {
-	Integer addEmployee(EmployeeBean employeeBean) throws Exception;
-	EmployeeBean getEmployeeDetails(Integer id)throws Exception;
-}
